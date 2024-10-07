f37ae07a38571c676f2f554914f4ebc97616a9ad
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-10-07 09:20:49 +0300

    v1.3

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button, Navbar } from "flowbite-react";
+import { Navbar } from "flowbite-react";
 import Link from "next/link";
 import Image from "next/image";
 export default function NavigationBar() {
@@ -12,6 +12,7 @@ export default function NavigationBar() {
             src="https://flowbite.com/docs/images/logo.svg"
             className="mr-3 h-6 sm:h-9"
             alt="Flowbite Logo"
+            width={20}
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Flowbite
