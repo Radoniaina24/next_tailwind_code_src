2af763f6304bc371038180444b4ad15429738746
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-10-08 16:28:06 +0300

    v1.6 "Validation register user"

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,6 +13,7 @@ export default function NavigationBar() {
             className="mr-3 h-6 sm:h-9"
             alt="Flowbite Logo"
             width={20}
+            height={20}
           />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Flowbite
--- src/features/auth/formRegisterUsers.tsx
+++ src/features/auth/formRegisterUsers.tsx
@@ -1,78 +1,70 @@
+"use client";
 import { Button } from "flowbite-react";
 import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import User from "@/interface/user";
+import Input from "@/components/form/Input";
+const UserSchema = yup.object({
+  name: yup.string().required("Ce nom est indispensable"),
+  email: yup
+    .string()
+    .email("Assurez-vous que le courriel est valide.")
+    .required("Ce courriel est requis."),
+  password: yup.string().required("Il est nécessaire d'avoir ce mot de passe"),
+});
+const initialValues: Omit<User, "_id"> = {
+  name: "",
+  email: "",
+  password: "",
+};
 export default function FormRegisterUsers() {
+  const formik = useFormik({
+    initialValues,
+    validationSchema: UserSchema,
+    onSubmit,
+  });
+  function onSubmit() {
+    console.log(values);
+    resetForm();
+  }
+  const { values, handleChange, handleSubmit, errors, touched, resetForm } =
+    formik;
   return (
-    <form className="max-w-sm mx-auto">
-      <div className="mb-5">
-        <label
-          htmlFor="repeat-password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
-          required
-        />
-      </div>
-
-      <div className="mb-5">
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-          required
-        />
-      </div>
-
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5 mr-1">
-          <input
-            id="terms"
-            type="checkbox"
-            value=""
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            required
-          />
-        </div>
-        <label
-          htmlFor="terms"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          I agree with the{" "}
-          <a
-            href="#"
-            className="text-blue-600 hover:underline dark:text-blue-500"
-          >
-            terms and conditions
-          </a>
-        </label>
-      </div>
+    <form
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      className="max-w-sm mx-auto"
+    >
+      <Input
+        type="text"
+        label="Name"
+        id="name"
+        value={values.name}
+        onChange={handleChange}
+        error={errors.name}
+        touched={touched.name}
+      />
+      <Input
+        type="email"
+        label="Email"
+        id="email"
+        value={values.email}
+        onChange={handleChange}
+        error={errors.email}
+        touched={touched.email}
+      />
+      <Input
+        type="password"
+        label="Mot de passe"
+        id="password"
+        value={values.password}
+        onChange={handleChange}
+        error={errors.password}
+        touched={touched.password}
+      />
       <Button type="submit" size={"sm"} className="bg-blue-600">
-        Register new account
+        Register account
       </Button>
     </form>
   );
